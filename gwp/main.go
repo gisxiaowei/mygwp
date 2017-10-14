9ca0fd0bc0ef89ea08215017db8a2adffd0ec515
gisxiaowei <gisxiaowei@localhost.localdomain>
2017-10-14 11:38:30 +0800

    加载配置文件，初始化日志

--- gwp/main.go
+++ gwp/main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func main() {
+	p("GWP", version(), "端口", config.Address)
+
 	// 处理静态资源
 	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("public"))
+	files := http.FileServer(http.Dir(config.Static))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
 	// 启动服务器
 	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
+		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
+		MaxHeaderBytes: 1 << 20,
 	}
 	server.ListenAndServe()
 }
---
+++ gwp/utils.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
+var logger *log.Logger
+
+func init() {
+	loadConfig()
+	initLogger()
+}
+
+// 加载配置文件
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("不能打开配置文件", err)
+	}
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalln("不能从配置文件中获取配置项", err)
+	}
+}
+
+// 初始化日志管理器
+func initLogger() {
+	file, err := os.OpenFile("mygwp.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("打开日志文件失败", err)
+	}
+	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+// 输出
+func p(a ...interface{}) {
+	fmt.Println(a)
+}
+
+// 版本
+func version() string {
+	return "0.1"
+}
+
+// 日志
+func info(args ...interface{}) {
+	logger.SetPrefix("INFO ")
+	logger.Println(args...)
+}
+
+func danger(args ...interface{}) {
+	logger.SetPrefix("ERROR ")
+	logger.Println(args...)
+}
+
+func warning(args ...interface{}) {
+	logger.SetPrefix("WARNING ")
+	logger.Println(args...)
+}
